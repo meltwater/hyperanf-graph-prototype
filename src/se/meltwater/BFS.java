6bb412b351d086496b4045adec3de8b112a2b87f
Johan Nilsson Hansen <granen@hotmail.com>
2016-02-26 14:44:59 +0100

    Vertex cover now prints progress feedback

--- src/se/meltwater/BFS.java
+++ src/se/meltwater/BFS.java
@@ -1,7 +1,66 @@
 package se.meltwater;
 
+import it.unimi.dsi.webgraph.ImmutableGraph;
+import it.unimi.dsi.webgraph.LazyIntIterator;
+import it.unimi.dsi.fastutil.ints.IntArrayFIFOQueue;
+import it.unimi.dsi.logging.ProgressLogger;
+
+import java.io.IOException;
+import java.util.Arrays;
+
 /**
  * Created by johan on 2016-02-24.
  */
 public class BFS {
+
+    public static void main(String[] args) throws IOException {
+        String graphFileName = "/home/johan/programming/master/it/unimi/dsi/webgraph/graphs/it-2004";
+        final ProgressLogger pl = new ProgressLogger();
+        final ImmutableGraph graph = ImmutableGraph.load( graphFileName, pl );
+
+        int n = graph.numNodes();
+
+        final int lo = 0;
+        final long hi = 100;
+
+        int curr = lo, succ, ecc = 0, reachable = 0;
+
+        final int maxDist = 7;
+
+        final IntArrayFIFOQueue queue = new IntArrayFIFOQueue();
+        final int[] dist = new int[ n ];
+        Arrays.fill( dist, Integer.MAX_VALUE );
+
+        long start = System.currentTimeMillis();
+        for( int i = lo; i < hi; i++ ) {
+            if ( dist[ i ] == Integer.MAX_VALUE ) { // Not already visited
+                queue.enqueue( i );
+
+                dist[ i ] = 0;
+
+                LazyIntIterator successors;
+
+                while( ! queue.isEmpty() ) {
+                    curr = queue.dequeueInt();
+                    successors = graph.successors( curr );
+                    int d = graph.outdegree( curr );
+                    while( d-- != 0 ) {
+                        succ = successors.nextInt();
+                        if ( dist[ succ ] == Integer.MAX_VALUE && dist[ curr ] + 1 <= maxDist ) {
+                            reachable++;
+                            dist[ succ ] = dist[ curr ] + 1;
+                            ecc = Math.max( ecc, dist[ succ ] );
+                            queue.enqueue( succ );
+                        }
+                    }
+                }
+            }
+            pl.update();
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println("Running time: " + (float)(end - start)/1000 );
+
+    }
+
 }
--- src/se/meltwater/VertexCover.java
+++ src/se/meltwater/VertexCover.java
@@ -1,5 +1,6 @@
 package se.meltwater;
 
+import com.martiansoftware.jsap.*;
 import com.sun.org.apache.xpath.internal.SourceTree;
 import it.unimi.dsi.big.webgraph.ImmutableGraph;
 import it.unimi.dsi.big.webgraph.LazyIntIterator;
@@ -9,6 +10,7 @@ import it.unimi.dsi.logging.ProgressLogger;
 
 import java.io.IOException;
 import java.util.*;
+
 
 /**
  * Created by johan on 2016-02-23.
@@ -21,16 +23,24 @@ public class VertexCover {
 
     private HashMap<Integer, Integer> maximalMatching = new HashMap<>();
     private BitSet vertexCover = new BitSet();
+    private final ProgressLogger pl;
+
+    private int progress = 0;
+
+    private static int updateInterval = 1000000;
 
     public VertexCover(String graphFileName) throws IOException {
-        final ProgressLogger pl = new ProgressLogger();
-        final ImmutableGraph graph = ImmutableGraph.load( graphFileName, pl );
+        pl = new ProgressLogger();
+        final ImmutableGraph graph = ImmutableGraph.loadMapped( graphFileName, pl );
         numberOfNodes = graph.numNodes();
-
         nodeIterator = graph.nodeIterator(0);
+
+        pl.expectedUpdates = numberOfNodes;
     }
 
-    public void fetchEdgesFromFile() throws Exception {
+    public void fetchEdgesFromFileAndCalculateVC() throws Exception {
+        pl.start();
+
         for( int currentNode = 0; currentNode < numberOfNodes; currentNode++ ) {
             nodeIterator.nextLong();
             LazyLongIterator successors;
@@ -39,9 +49,18 @@ public class VertexCover {
 
             addEdges(currentNode, successors, degree);
         }
+
+        pl.stop();
     }
 
     private void addEdges(long from, LazyLongIterator successors, long numberOfSuccessors) {
+        if(progress > updateInterval) {
+            pl.updateAndDisplay(progress);
+            progress = 0;
+        }
+        progress++;
+
+
         if(numberOfSuccessors == 0 || vertexCover.get((int)from)) {
             return;
         }
@@ -60,16 +79,29 @@ public class VertexCover {
             }
             successorsLeft--;
         }
+
+
     }
 
 
     public static void main(String[] args) throws Exception {
+
         String graphFileName = "/home/johan/programming/master/it/unimi/dsi/webgraph/graphs/uk-2002";
+        /*SimpleJSAP jsap = new SimpleJSAP( VertexCover.class.getName(), "Compresses differentially a graph. Source and destination are basenames from which suitable filenames will be stemmed; alternatively, if the suitable option was specified, source is a spec (see below). For more information about the compression techniques, see the Javadoc documentation.",
+                new Parameter[] {
+                        new FlaggedOption( "path", JSAP.STRING_PARSER, null, JSAP.NOT_REQUIRED, 'p', "path", "" ),
+                }
+        );
+
+        JSAPResult jsapResult = jsap.parse( args );
+        if ( jsap.messagePrinted() ) System.exit( 1 );
+
+        String graphFileName = jsapResult.getString( "path" );*/
 
         VertexCover vertexCover = new VertexCover(graphFileName);
 
         long start = System.currentTimeMillis();
-        vertexCover.fetchEdgesFromFile();
+        vertexCover.fetchEdgesFromFileAndCalculateVC();
         long end = System.currentTimeMillis();
 
         int maximalMatchingSize = vertexCover.maximalMatching.size();
@@ -77,11 +109,9 @@ public class VertexCover {
         long nodesInGraph = vertexCover.numberOfNodes;
 
         System.out.println("Maximal matching of size: " + maximalMatchingSize);
-        System.out.println("Vertex cover of size: " + vertexCoverSize
-                + " and originally was: " + nodesInGraph);
-        System.out.println(" giving a efficiency rate of " +
-                (double)vertexCoverSize / nodesInGraph) ;
-        System.out.println("Elapsed time: " + (float)(end - start)/1000);
+        System.out.println("Vertex cover of size: " + vertexCoverSize + " : " + nodesInGraph);
+        System.out.println("Efficiency rate of " + (double)vertexCoverSize / nodesInGraph) ;
+        System.out.println("Elapsed time: " + (float)(end - start)/1000 + "s");
     }
 }
 
