f16acda28ac40fb63b6a90c86a4fa95a643557e6
Johan Nilsson Hansen <granen@hotmail.com>
2016-04-06 15:55:55 +0200

    The DVC insertion plot now use the it-2004 graph

--- scripts/benchmarksdvcplot.m
+++ scripts/benchmarksdvcplot.m
@@ -1,6 +1,6 @@
 data = load ("../benchmarksdvc.data");
 
-nrBreaks = 10;
+nrBreaks = 1;
 x  = data(:,1)';
 y1 = data(:,2)';
 y2 = data(:,3)';
@@ -9,11 +9,12 @@ y3 = data(:,4)';
 
 [ax, h1, h2] = plotyy (x, y3, x, polyval(y2polf, x));
 
+title("It-2004");
 set(h2, "Linestyle", "--");
 set(h1,'Linewidth', 1.5); 
 set(h2,'Linewidth', 1.5); 
-set(ax(1), 'ylim', [0 100])
-set(ax(2), 'ylim', [0 3])
+set(ax(1), 'ylim', [0 50])
+set(ax(2), 'ylim', [0 4])
 xlabel ("Inserted edges (Millions)");
 ylabel (ax(1), "Elapsed time (Seconds)");
 ylabel (ax(2), "Heap-size (GigaByte)");
