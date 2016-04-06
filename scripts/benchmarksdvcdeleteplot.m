e647cdc8fb168396dc0df60231a231e7e6dadbb2
Johan Nilsson Hansen <granen@hotmail.com>
2016-04-06 16:00:33 +0200

    Added script for plotting dvc deletions benchmark

---
+++ scripts/benchmarksdvcdeleteplot.m
@@ -0,0 +1,22 @@
+data = load ("../benchmarksDvcDeletionsReal.data");
+
+nrBreaks = 2;
+x  = data(:,1)'.*1000;
+y1 = data(:,2)';
+y2 = data(:,3)';
+y2polf = polyfit(x, y2, nrBreaks);
+y3 = data(:,4)';
+
+[ax, h1, h2] = plotyy (x, y3, x, polyval(y2polf, x));
+
+title("In-2004");
+set(h2, "Linestyle", "--");
+set(h1,'Linewidth', 1.5); 
+set(h2,'Linewidth', 1.5); 
+set(ax(1), 'ylim', [0 110])
+set(ax(2), 'ylim', [0 4])
+xlabel ("Inserted edges (Thousands)");
+ylabel (ax(1), "Elapsed time (Seconds)");
+ylabel (ax(2), "Heap-size (GigaByte)");
+
+legend ([h1, h2], {"Time", "Memory"}) 
