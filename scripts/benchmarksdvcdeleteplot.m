26f1d6d272bcf7419d2609b5b083f4f1dcdc4a3f
Johan Nilsson Hansen <granen@hotmail.com>
2016-04-08 10:18:59 +0200

    Added benchmark for bfs vs msbfs

--- scripts/benchmarksdvcdeleteplot.m
+++ scripts/benchmarksdvcdeleteplot.m
@@ -13,7 +13,7 @@ title("It-2004");
 set(h2, "Linestyle", "--");
 set(h1,'Linewidth', 1.5); 
 set(h2,'Linewidth', 1.5); 
-set(ax(1), 'ylim', [0 110])
+set(ax(1), 'ylim', [0 1000])
 set(ax(2), 'ylim', [0 4])
 xlabel ("Deleted edges (Million)");
 ylabel (ax(1), "Elapsed time (Seconds)");
--- src/se/meltwater/examples/Benchmarks.java
+++ src/se/meltwater/examples/Benchmarks.java
@@ -1,16 +1,20 @@
 package se.meltwater.examples;
 
 
+import com.google.common.collect.Range;
 import it.unimi.dsi.big.webgraph.ImmutableGraph;
 import it.unimi.dsi.big.webgraph.BVGraph;
 import it.unimi.dsi.big.webgraph.ImmutableSequentialGraph;
 import it.unimi.dsi.big.webgraph.Transform;
 import se.meltwater.algo.DANF;
 import se.meltwater.algo.HyperBoll;
+import se.meltwater.bfs.MSBreadthFirst;
+import se.meltwater.bfs.StandardBreadthFirst;
 import se.meltwater.graph.Edge;
 import se.meltwater.graph.IGraph;
 import se.meltwater.graph.ImmutableGraphWrapper;
 import se.meltwater.graph.SimulatedGraph;
+import se.meltwater.utils.Utils;
 import se.meltwater.vertexcover.DynamicVertexCover;
 
 import java.io.FileNotFoundException;
@@ -18,8 +22,11 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.BitSet;
 import java.util.Collections;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 /**
  * @author Simon Lindhén
@@ -29,11 +36,86 @@ import java.util.concurrent.ThreadLocalRandom;
  */
 public class Benchmarks {
 
-    private static float chanceNewNode = 1.05f;
+    private static float chanceNewNode = 1.01f;
     private static long bytesPerGigaByte = 1024 * 1024 * 1024;
 
     private static int added = 0;
     private static long lastTime;
+
+
+    /**
+     * Loads a real graph and performs both a standard bfs and a msbfs on it.
+     * Their completion time is measured and saved into a data file.
+     * The BFS's will originate from the first nodes in the graph.
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public static void benchmarkStdBfs() throws IOException, InterruptedException {
+        final int numberOfSources = 5000;
+        final int startNode = 5000;
+        final int maxSteps = 10;
+        final String graphFile = "testGraphs/in-2004";
+        final String dataFile = "benchmarkBfs.data";
+
+        ImmutableGraphWrapper graph = new ImmutableGraphWrapper(ImmutableGraph.load(graphFile));
+        long[] sources = LongStream.range(startNode, startNode + numberOfSources).toArray();
+
+        PrintWriter writer = new PrintWriter(dataFile);
+        writer.println("#h nrSources stdbfsMillis msbfsMillis");
+
+        for (int h = 0; h <= maxSteps; h++) {
+            long stdTotalTime = performStandardBfsAndMeasureTime(sources, graph, h);
+            long msbfsTotalTime = performMSBfsAndMeasureTime(sources, graph, h);
+
+            System.out.println("Iteration " + h + " completed.");
+            writer.println(h + " " + sources.length + " " + stdTotalTime + " " + msbfsTotalTime);
+        }
+
+        writer.close();
+    }
+
+    /**
+     * Runs a std bfs from the sources in the current graph. The bfs will stop after h levels.
+     * @param sources
+     * @param graph
+     * @param h
+     * @return The elapsed time in millis
+     */
+    private static long performStandardBfsAndMeasureTime(long[] sources, IGraph graph, int h) {
+        long startTime = System.currentTimeMillis();
+        StandardBreadthFirst.breadthFirstSearch(sources, graph, h);
+        long endTime = System.currentTimeMillis();
+        return endTime - startTime;
+    }
+
+    /**
+     *
+     * @param sources
+     * @param graph
+     * @param h
+     * @return
+     * @throws InterruptedException
+     */
+    private static long performMSBfsAndMeasureTime(long[] sources, IGraph graph, final int h) throws InterruptedException {
+        //Dummy traveler
+        MSBreadthFirst.Traveler traveler = (MSBreadthFirst.Traveler t1, int depth) ->  t1;
+        MSBreadthFirst.Traveler[] travelers = Utils.repeat(traveler,sources.length, new MSBreadthFirst.Traveler[0]);
+
+        MSBreadthFirst.Visitor visitor = (long node, BitSet bfsVisits, BitSet seen, int depth, MSBreadthFirst.Traveler t) -> {
+            if(depth == h) {
+                bfsVisits.clear();
+            }
+        };
+
+        long startTime = System.currentTimeMillis();
+        MSBreadthFirst msbfs = new MSBreadthFirst(sources, travelers ,graph,visitor);
+        msbfs.breadthFirstSearch();
+        long endTime = System.currentTimeMillis();
+        return endTime - startTime;
+    }
+
+
+
 
 
     public static void benchmarkUnionVsStored() throws IOException {
@@ -210,10 +292,13 @@ public class Benchmarks {
         int log2m = 7;
         int h = 3;
         int edgesToAdd = 5000000;
-        int bulkSize = 50000;
+        int bulkSize =      5000;
 
-        SimulatedGraph graph = new SimulatedGraph();
-        graph.addNode(0);
+        //SimulatedGraph graph = new SimulatedGraph();
+        //graph.addNode(0);
+
+        System.out.println("Loading graph");
+        IGraph graph = new ImmutableGraphWrapper(BVGraph.loadMapped("testGraphs/it-2004"));
 
         DANF danf = runHyperBoll(log2m, h, graph);
 
@@ -225,6 +310,7 @@ public class Benchmarks {
 
         Edge[] edges = new Edge[bulkSize];
 
+        System.out.println("Starting edge insertions");
         while(System.in.available() == 0 && added < edgesToAdd) {
             generateEdges(graph.getNumberOfNodes(), bulkSize, edges);
 
@@ -329,13 +415,18 @@ public class Benchmarks {
         }
     }
 
-    private static DANF runHyperBoll(int log2m, int h, SimulatedGraph graph) throws IOException {
+    private static DANF runHyperBoll(int log2m, int h, IGraph graph) throws IOException {
+        System.out.println("Calculating VC");
         DynamicVertexCover dvc = new DynamicVertexCover(graph);
+
+        System.out.println("Calculating Transpose");
         DANF danf = new DANF(dvc, h, graph);
 
+        System.out.println("Starting HyperBall");
         HyperBoll hyperBoll = new HyperBoll(graph, log2m);
         hyperBoll.init();
         for (int i = 1; i <= h; i++) {
+            System.out.println("HyperBoll iteration: " + i);
             hyperBoll.iterate();
             danf.addHistory(hyperBoll.getCounter(), i);
         }
@@ -349,7 +440,8 @@ public class Benchmarks {
         //Benchmarks.benchmarkDVCInsertionsSimluated();
         //Benchmarks.benchmarkDVCInsertionsReal();
         //Benchmarks.benchmarkDVCDeletionsSimulated();
-        Benchmarks.benchmarkDVCDeletionsReal();
+        //Benchmarks.benchmarkDVCDeletionsReal();
         //Benchmarks.benchmarkUnionVsStored();
+        Benchmarks.benchmarkStdBfs();
     }
 }
