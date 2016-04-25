c9cbddc68a8967e5bd79561087a6e6f94ee3fe1d
Johan Nilsson Hansen <granen@hotmail.com>
2016-04-25 10:03:53 +0200

    Top counters are now sorted and only nodes above a certain danf value are considered

--- src/main/java/se/meltwater/algo/TopNodeCounter.java
+++ src/main/java/se/meltwater/algo/TopNodeCounter.java
@@ -6,6 +6,8 @@ import se.meltwater.graph.IGraph;
 import se.meltwater.vertexcover.IDynamicVertexCover;
 
 import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * @author Simon Lindhén
@@ -20,12 +22,19 @@ public class TopNodeCounter {
     private TreeSet<Pair<Double, Long>> nodesSortedByValue;
     private HashMap<Long, Double> updatedNodesWithValue;
     private long timeOfLastUpdate;
-    private static final long UPDATE_INTERVAL_MS = 1000;
-    private static final double CHANGE_THREASHOLD_PERCENTAGE = 0.1;
+    private static final long UPDATE_INTERVAL_MS = 1000 * 10;
+    private static final double CHANGE_THREASHOLD_PERCENTAGE = 1.5;
+    private static final double MIN_NODE_COUNT = 10;
+    private Consumer<Set<Pair<Double, Long>>> rapidChangeCallback;
 
     public TopNodeCounter(DANF danf) {
         this.danf = danf;
         initNodeSets(danf.getGraph());
+        rapidChangeCallback = null;
+    }
+
+    public void setRapidChangeCallback( Consumer<Set<Pair<Double, Long>>> rapidChangeCallback) {
+        this.rapidChangeCallback = rapidChangeCallback;
     }
 
     private void initNodeSets(IGraph graph) {
@@ -37,19 +46,22 @@ public class TopNodeCounter {
         }
     }
 
-    public Map<Long, Double> updateNodeSets(Edge ... edges) {
+    public void updateNodeSets(Edge ... edges) {
         insertUpdatedValues(edges);
         long currentTime = System.currentTimeMillis();
-        Map<Long, Double> nodesWithPercentageChange = new HashMap<>();
+        //TreeMap<Double, Long> nodesWithPercentageChange = new TreeMap<Double, Long>();
+        TreeSet<Pair<Double, Long>> nodesWithPercentageChange = new TreeSet<Pair<Double, Long>>(nodeScoreComparator());
         if(currentTime - timeOfLastUpdate > UPDATE_INTERVAL_MS) {
             mergeNodeSets(nodesWithPercentageChange);
             timeOfLastUpdate = currentTime;
-        }
 
-        return nodesWithPercentageChange;
+            if(rapidChangeCallback != null) {
+                rapidChangeCallback.accept(nodesWithPercentageChange);
+            }
+        }
     }
 
-    private void mergeNodeSets(Map<Long, Double> nodesWithPercentageChange) {
+    private void mergeNodeSets(TreeSet<Pair<Double, Long>> nodesWithPercentageChange) {
         Iterator<Pair<Double, Long>> it = nodesSortedByValue.iterator();
         while(it.hasNext()) {
             Pair<Double, Long> pair = it.next();
@@ -58,7 +70,7 @@ public class TopNodeCounter {
                 double previousValue = pair.getKey();
                 double valueChange = currentValue / previousValue;
                 if(valueChange > CHANGE_THREASHOLD_PERCENTAGE) {
-                    nodesWithPercentageChange.put(pair.getValue(), valueChange);
+                    nodesWithPercentageChange.add(new Pair(valueChange, pair.getValue()));
                 }
 
                 it.remove();
@@ -76,8 +88,16 @@ public class TopNodeCounter {
     private void insertUpdatedValues(Edge ... edges) {
         for (int i = 0; i < edges.length; i++) {
             Edge edge = edges[i];
-            updatedNodesWithValue.put(edge.from, danf.count(edge.from, danf.getMaxH()));
-            updatedNodesWithValue.put(edge.to,   danf.count(edge.to,   danf.getMaxH()));
+
+            double fromCount = danf.count(edge.from, danf.getMaxH());
+            if(fromCount > MIN_NODE_COUNT) {
+                updatedNodesWithValue.put(edge.from, fromCount);
+            }
+
+            double toCount = danf.count(edge.to, danf.getMaxH());
+            if(toCount > MIN_NODE_COUNT) {
+                updatedNodesWithValue.put(edge.to, toCount);
+            }
         }
     }
 
