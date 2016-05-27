ad2ca26dd795d3e9155e7c58b9ee8f564246a8ff
Johan Nilsson Hansen <granen@hotmail.com>
2016-05-27 14:23:20 +0200

    Refactored out DanfVsHanf benchmark

---
+++ src/main/java/se/meltwater/benchmarks/BenchmarkUtils.java
@@ -0,0 +1,44 @@
+package se.meltwater.benchmarks;
+
+import se.meltwater.graph.Edge;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Created by johan on 2016-05-27.
+ */
+public class BenchmarkUtils {
+
+    final public static String graphFolder = "testGraphs/";
+    final public static String dataFolder = "files/";
+
+    /**
+     * Generates a bulk of random edges.
+     * @param maxNewNode The max node id of the generated edges
+     * @param bulkSize The number of edges to generate
+     */
+    public static Edge[] generateEdges(long maxNewNode, int bulkSize) {
+        Edge[] edges = new Edge[bulkSize];
+
+        for (int i = 0; i < bulkSize; i++) {
+            long from = ThreadLocalRandom.current().nextLong(maxNewNode );
+            long to = ThreadLocalRandom.current().nextLong(maxNewNode );
+            edges[i] = new Edge(from, to);
+        }
+
+        return edges;
+    }
+
+    /**
+     * Returns the current time as date
+     * @return
+     */
+    private static String getDateString() {
+        DateFormat dateFormat = new SimpleateFormat("yyyy-MM-dd-HH:mm:ss");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+}
